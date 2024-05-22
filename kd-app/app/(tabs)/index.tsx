28c6ba3fbd538d93b9a3d3270de95474cff158e0
Kendrick Cruz <kendrick22.kc@gmail.com>
2024-05-22 15:36:34 +0800

    Input Header

--- kd-app/app/(tabs)/index.tsx
+++ kd-app/app/(tabs)/index.tsx
@@ -1,11 +1,25 @@
-
 import App from '@/components/App';
+import { Header } from '@/components/Header';
+import { Input } from '@/components/Input';
+import { TasksList } from '@/components/Tasklist';
+import { StyleSheet, View } from 'react-native';
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EBEBEB'
+  }
+})
 
 export default function HomeScreen() {
   return (
-
-    <App />
+    <View style={styles.container}>
+      <Header />
+      <Input />
+    </View>
   );
 }
+
 
 
---
+++ kd-app/components/Input.tsx
@@ -0,0 +1,73 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, TouchableOpacity, View, Image } from 'react-native';
+
+interface Input {
+    addTask: (task: string) => void;
+}
+
+export function Input({ addTask }: Input) {
+    const [task, setTask] = useState('');
+
+    function handleAddNewTask() {
+        if (!task) {
+            return;
+        }
+        addTask(task);
+        setTask('');
+    }
+
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.input}
+                placeholder="Add Task"
+                placeholderTextColor="#B2B2B2"
+                returnKeyType="send"
+                selectionColor="#666666"
+                value={task}
+                onChangeText={setTask}
+                onSubmitEditing={handleAddNewTask}
+            />
+            <TouchableOpacity
+                testID="add-new-task-button"
+                activeOpacity={0.7}
+                style={styles.addButton}
+                onPress={handleAddNewTask}
+            >
+                <AntDesign name="plus" size={24} color="black" />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        backgroundColor: '#FFF',
+        borderRadius: 5,
+        marginTop: -28,
+        marginHorizontal: 24,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    input: {
+        flex: 1,
+        height: 56,
+        paddingHorizontal: 20,
+        backgroundColor: '#FFF',
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius: 5,
+        borderRightWidth: 1,
+        borderRightColor: '#EBEBEB',
+        color: '#666666'
+    },
+    addButton: {
+        backgroundColor: '#FFF',
+        height: 56,
+        paddingHorizontal: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderTopRightRadius: 5,
+        borderBottomRightRadius: 5,
+    },
+});
